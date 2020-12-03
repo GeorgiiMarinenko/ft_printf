7ed9a36c43f52d9a586e1342428ea1d6d510deea
G.Marinenko <Gmarinenko@mail.ru>
2020-12-04 00:38:13 +0300

    make print_dec_paddinf smaller

--- print_dec_padding.c
+++ print_dec_padding.c
@@ -6,7 +6,7 @@
 /*   By: georgy <georgy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 12:11:54 by georgy            #+#    #+#             */
-/*   Updated: 2020/12/04 00:34:50 by georgy           ###   ########.fr       */
+/*   Updated: 2020/12/04 00:37:02 by georgy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,15 +43,9 @@ void	ft_padding_right_align(int nbrlen, int n, t_args *f)
 			if ((f->f_zero && f->f_width != f->precision) || n == 1)
 			{
 				if (f->f_width && f->f_precision)
-				{
-					// f->f_width = (f->precision > nbrlen && !repit++ && n == 1) ? f->f_width-1 : f->f_width;
-					if (f->precision > nbrlen && !repit++ && n == 1)
-						f->f_width -= 1;
-				}
-				f->len += write(f->file_descr, " ", 1);
+					f->f_width = (f->precision > nbrlen && !repit++ && n == 1) ? f->f_width-1 : f->f_width;
 			}
-			else
-				f->len += write(f->file_descr, " ", 1);
+			f->len += write(f->file_descr, " ", 1);
 		}
 	}
 }
